ff25d9b0993253cf3058ae9133dbe66a6822b2f0
Max Qian <public@maxqia.com>
2020-03-19 01:06:45 -0700

    Cursor

--- typer.js
+++ typer.js
@@ -1,13 +1,17 @@
 var i = 0;
 var speed = 100; /* The speed/duration of the effect in milliseconds */
+var block = "█";
 var element = document.getElementById("typewriter");
 var txt = element.innerHTML;
-element.innerHTML = null;
-typeWriter();
+var curtxt = "";
+element.innerHTML = block;
+setTimeout(typeWriter, speed);
 
 function typeWriter() {
   if (i < txt.length) {
-        element.innerHTML += txt.charAt(i);
+        console.log(i);
+        curtxt += txt.charAt(i);
+        element.innerHTML = curtxt + block;
         i++;
         
         var nextSpeed = speed;
