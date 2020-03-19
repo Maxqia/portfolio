6d927d114658caceb5e56de20e5cb490541a1317
Max Qian <public@maxqia.com>
2020-03-19 01:32:20 -0700

    Blinky Cursor

--- typer.js
+++ typer.js
@@ -4,20 +4,40 @@ var block = "█";
 var element = document.getElementById("typewriter");
 var txt = element.innerHTML;
 var curtxt = "";
+
+var blinkTime = 600; // in ms
+var blinkInterval = null;
 element.innerHTML = block;
 setTimeout(typeWriter, speed);
 
 function typeWriter() {
   if (i < txt.length) {
-        console.log(i);
-        curtxt += txt.charAt(i);
-        element.innerHTML = curtxt + block;
-        i++;
-        
-        var nextSpeed = speed;
-        if (txt.charAt(i) == ' ') nextSpeed += 35;
-        setTimeout(typeWriter, nextSpeed);
+      curtxt += txt.charAt(i);
+      element.innerHTML = curtxt + block;
+      i++;
+
+      var nextSpeed = speed;
+      if (txt.charAt(i) == ' ') nextSpeed += 35;
+      setTimeout(typeWriter, nextSpeed);
   } else {
-    
+      blinkInterval = setInterval(blinkCursor, blinkTime);
+      setTimeout(stopBlink, blinkTime)
+  }
+}
+
+var blink = false;
+var blinkCount = 1; // starts off blinking
+function blinkCursor() {
+  console.log(blink);
+  if(blink) {
+    element.innerHTML = curtxt + block;
+    blinkCount++;
+  } else {
+    element.innerHTML = curtxt;
+  }
+  blink = !blink;
+  
+  if(blinkCount >= 10) { // stop at 10 (gnome does this if you're wondering where it came from)
+    clearInterval(blinkInterval);
   }
 }
