ada3a06e54cebde6a0de416524012b55bf01a1b0
Max Qian <public@maxqia.com>
2020-03-20 14:44:14 -0700

    Make Navbar accurately reflect the current pages

--- functions.php
+++ functions.php
@@ -4,15 +4,20 @@ if ( ! function_exists( 'portfolio_setup' ) ):
 
 function portfolio_setup() {
   /*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
+   * Let WordPress manage the document title.
+   * By adding theme support, we declare that this theme does not use a
+   * hard-coded <title> tag in the document head, and expect WordPress to
+   * provide it for us.
+   */
+  add_theme_support( 'title-tag' );
+
+  // This theme uses wp_nav_menu() in one location.
+  register_nav_menus( array(
+    'header-menu' => __( 'Header Menu' ),
+  ));
 }
 
 endif;
-
 add_action( 'after_setup_theme', 'portfolio_setup' );
+
 ?>
--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory'); ?>/title.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory'); ?>/style.css">
     <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_directory'); ?>/index.css">
 
     <?php wp_head(); ?>
@@ -23,16 +23,19 @@
 <?php endif; ?>
         <div id="navbar">
           <h2>
-            <?php if (!is_front_page()): ?>
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-            <?php endif; ?>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                rel="home"
+                style="<?php echo is_front_page() ? "visibility: hidden;" : ""?>"
+            >
+              <?php bloginfo( 'name' ); ?>
+            </a>
           </h2>
-          <ul id="navlist">
-             <li><div class="current"><a href="">Main Page</a></div></li>
-             <li><div><a href="games.html" class="">Games</a></div></li>
-             <li><div><a href="https://maxqia.wordpress.com" class="">Blog</a></div></li>
-             <li><div><a href="#contact" class="">Contact</a></div></li>
-          </ul>
+          <?php wp_nav_menu( array(
+            'theme_location' => 'header-menu',
+            'container' => false,
+            'fallback_cb' => false,
+            'menu_id' => 'navlist',
+          )) ?>
         </div>
 <?php if (is_front_page()): ?>
     </div> <!-- #navcontain -->
